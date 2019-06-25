c2952d6634f51ddec8b4e8a26b829622576cbfb5
Lucia Rodriguez <774@holbertonschool.com>
2019-06-25 04:29:52 +0000

    [Update] Uploading the 102 task

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,43 @@
-#!/usr/bin/env bash
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+/**
+ *main - create zombie processes
+ *Return: 0 if sucessful
+ */
+int main(void)
+{
+	int itera = 0;
+	pid_t nino;
+
+	while (itera < 5)
+	{
+		nino = fork();
+		if (nino > 0)
+		{
+			sleep(1);
+		}
+		else
+		{
+			exit(0);
+		}
+		printf("Zombie process created, PID: %d\n", nino);
+		itera++;
+	}
+	infinite_while();
+	return (0);
+
+}
+/**
+ *infinite_while - The name explains itself
+ *Return: 0 if sucessful
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
