646d0ff7d5b6f133b7d18ac63171184bd45a08fc
Lucia Rodriguez <774@holbertonschool.com>
2019-08-08 11:47:44 -0500

    [Update] Uploading the 4th task

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # This script adds a new line in the config file for ssh
 exec {'appendingpasswordoff':
-command  => "echo -e 'IdentityFile ~/.ssh/holberton\nPasswordAuthentication no' >> /etc/ssh/ssh_config",
-provider => 'shell',
+command  => '/bin/echo -e "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
+provider => 'shell';
 }
