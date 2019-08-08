5c70e72f63f5f873e67d31f3cf92eec9d9174593
Lucia Rodriguez <774@holbertonschool.com>
2019-08-08 11:40:32 -0500

    [Update] Uploading the 4th file

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,5 @@
 # This script adds a new line in the config file for ssh
-file { '~/.ssh/config':
-ensure => 'present',
-}
-
-file_line { 'appendingkey':
-path => '~/.ssh/config',
-line => 'IdentityFile ~/.ssh/holberton',
-}
-
-file_line { 'appendingpasswordoff':
-path => '~/.ssh/config',
-line => 'PasswordAuthentication no',
+exec {'appendingpasswordoff':
+command  => "echo -e 'IdentityFile ~/.ssh/holberton\nPasswordAuthentication no' >> /etc/ssh/ssh_config",
+provider => 'shell',
 }
