e92a8ae33a6df7c38a669b1b42ceb978497a109e
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-17 03:08:29 +0000

    [Fixing importing order, documentation and preventing execution if imported]

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-from sys import argv
+""" This script gets info from an API"""
 import requests
+from sys import argv
 
-userdata = {"id": argv[1]}
-usertodo = {"userId": argv[1]}
-userurl = "https://jsonplaceholder.typicode.com/users"
-todourl = "https://jsonplaceholder.typicode.com/todos"
-user = requests.get(userurl, params=userdata)
-todo = requests.get(todourl, params=usertodo)
-usuario = user.json()
-remain = todo.json()
-total = len(remain)
-counter = 0
-for itera in remain:
-    if itera.get("completed") is True:
-        counter += 1
-print("Employee {} is done with tasks({}/{}):".format(usuario[0]["name"],
-                                                      counter, total))
-for counter in remain:
-    if counter.get("completed") is True:
-        print("\t{}".format(counter["title"]))
+if __name__ == "__main__":
+    userdata = {"id": argv[1]}
+    usertodo = {"userId": argv[1]}
+    userurl = "https://jsonplaceholder.typicode.com/users"
+    todourl = "https://jsonplaceholder.typicode.com/todos"
+    user = requests.get(userurl, params=userdata)
+    todo = requests.get(todourl, params=usertodo)
+    usuario = user.json()
+    remain = todo.json()
+    total = len(remain)
+    counter = 0
+    for itera in remain:
+        if itera.get("completed") is True:
+            counter += 1
+    print("Employee {} is done with tasks({}/{}):".format(usuario[0]["name"],
+                                                          counter, total))
+    for counter in remain:
+        if counter.get("completed") is True:
+            print("\t{}".format(counter["title"]))
