f755a418bbc846ea7f1beaf9d15696892a0cfe4f
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-17 04:55:00 +0000

    [Fix] Fixing format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+""" This script gets info from an API and exports to a JSON file"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    userdata = {"id": argv[1]}
+    usertodo = {"userId": argv[1]}
+    userurl = "https://jsonplaceholder.typicode.com/users"
+    todourl = "https://jsonplaceholder.typicode.com/todos"
+    user = requests.get(userurl, params=userdata)
+    todo = requests.get(todourl, params=usertodo)
+    usuario = user.json()
+    remain = todo.json()
+    total = len(remain)
+    subdict = {}
+    subdict["username"] = usuario[0].get("username")
+    for counter in remain:
+        for key, value in counter.items():
+            if key == "title" or key == "completed":
+                subdict["key"] = value
+    dicto = {}
+    listando = []
+    dicto[usuario[0].get("id")] = listando
+    listando.append(subdict)
+    with open('{}.json'.format(int(argv[1])), 'w') as writingfile:
+        json.dump(dicto, writingfile)
