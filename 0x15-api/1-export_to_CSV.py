9a736d4d4d5f3ebd45e5a1792885a6cedc593892
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-17 04:27:37 +0000

    [Feature]Uploading the first task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+""" This script gets info from an API"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    userdata = {"id": argv[1]}
+    usertodo = {"userId": argv[1]}
+    userurl = "https://jsonplaceholder.typicode.com/users"
+    todourl = "https://jsonplaceholder.typicode.com/todos"
+    user = requests.get(userurl, params=userdata)
+    todo = requests.get(todourl, params=usertodo)
+    usuario = user.json()
+    remain = todo.json()
+    total = len(remain)
+    counter = 0
+    lista = []
+    for itera in remain:
+        if itera.get("completed") is True:
+            counter += 1
+        appending = [usuario[0].get("id"), usuario[0].get("username"),
+                     itera.get("completed"), itera.get("title")]
+        lista.append(appending)
+    print("Employee {} is done with tasks({}/{}):".
+          format(usuario[0].get("name"), counter, total))
+    for counter in remain:
+        if counter.get("completed") is True:
+            print("\t {}".format(counter["title"]))
+    with open('USER_ID.csv', 'w', newline='') as writingfile:
+        writing = csv.writer(writingfile, delimiter=',', quotechar='"',
+                             quoting=csv.QUOTE_ALL)
+        for iteran in lista:
+            writing.writerow(iteran)
