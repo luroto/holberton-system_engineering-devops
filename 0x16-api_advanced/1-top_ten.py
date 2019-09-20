3ab10e89a30e6f0701cbe0c4d9677206a6d52d80
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-20 00:00:01 +0000

    [Feature] Uploading the first task

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This script prints the top ten hot post of a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """ This script gets and processes the http response"""
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    identify = {"User-Agent": "Requests library from Python",
+                "From": "774@holbertonschool.com"}
+    to_print = []
+    hot = requests.get(url, headers=identify)
+    if hot.status_code == 404:
+        return 0
+    hot = hot.json()
+    hot = hot["data"]
+    hot = hot["children"]
+    for items in hot:
+        del items["kind"]
+    for data in hot:
+        to_print.append(data["data"])
+    hot = to_print
+    to_print = []
+    for dictio in hot:
+        to_print.append(dictio["title"])
+    itera = 0
+    for itera in range(len(to_print)):
+        if itera < 9:
+            print(to_print[itera])
+        itera += 1
