8be942b48fc3ceefa4ea16df4e9f10f808fb79b2
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-10-03 03:47:42 +0000

    [Feature] Fixing . on file extension

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # This script fixes a line for the wordpress server that is not working properly
 exec { 'fixing a line':
-        command => 'usr/bin/sudo /bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+        command => 'usr/bin/sudo /bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
     }
